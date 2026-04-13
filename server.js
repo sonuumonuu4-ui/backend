b37746c5fde847879ff4ba6b47f5473bee42174c
sonuumonuu4-ui <sonuumonuu4@gmail.com>
2026-04-13 15:17:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ app.options('*', cors());
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 // ─── IN-MEMORY DATABASE ─────────────────────────────
 let students = [
   {
